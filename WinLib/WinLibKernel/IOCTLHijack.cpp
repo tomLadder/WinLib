d17a267300373d0fde913a0e5e24dbdcc995844c
Thomas Leiter <leitert@edu.htl-villach.at>
2018-01-31 19:50:08 +0100

    :construction: ioctlhijack

--- WinLib/WinLibKernel/IOCTLHijack.cpp
+++ WinLib/WinLibKernel/IOCTLHijack.cpp
@@ -1,9 +1,61 @@
 #include "IOCTLHijack.h"
 
-IOCTLHijack::IOCTLHijack(DRIVER_OBJECT *_victim) {
+IOCTLHijack::IOCTLHijack(DRIVER_OBJECT *_victim, CHAR _idx, IOCTLCallback _callback) {
+	this->idx = _idx;
+	this->callback = _callback;
 	this->victim = _victim;
 }
 
 BOOLEAN IOCTLHijack::isHijackable() {
 	return this->victim->MajorFunction[IRP_MJ_DEVICE_CONTROL] != nullptr;
+}
+
+BOOLEAN IOCTLHijack::watch() {
+	HANDLE threadHandle = NULL;
+	CLIENT_ID clientID = { 0 };
+	OBJECT_ATTRIBUTES obAttr = { 0 };
+	PVOID pThread = NULL;
+	OBJECT_HANDLE_INFORMATION handleInfo = { 0 };
+
+	union compilerHack {
+		VOID (*func)(IOCTLHijack* this_ptr);
+		PVOID addr;
+	};
+
+	compilerHack chack = { &IOCTLHijack::WatcherThread };
+
+	InitializeObjectAttributes(&obAttr, NULL, OBJ_KERNEL_HANDLE, NULL, NULL);
+	auto status = PsCreateSystemThread(&threadHandle, THREAD_ALL_ACCESS, &obAttr, NULL, &clientID, (PKSTART_ROUTINE)chack.addr, this);
+
+	if (!NT_SUCCESS(status)) {
+		return FALSE;
+	}
+
+	status = ObReferenceObjectByHandle(threadHandle, THREAD_ALL_ACCESS, *PsThreadType, KernelMode, &pThread, &handleInfo);
+
+	if (NT_SUCCESS(status)) {
+		KeWaitForSingleObject(pThread, Executive, KernelMode, TRUE, NULL);
+	}
+
+	if (pThread) {
+		ObDereferenceObject(pThread);
+	}
+
+	return TRUE;
+}
+
+VOID IOCTLHijack::WatcherThread(IOCTLHijack* obj) {
+	UNREFERENCED_PARAMETER(obj);
+
+	int i = 0;
+
+	while (TRUE) {
+		i++;
+		PRINT("=> Hello From WatcherThread");
+
+		if (i == 10)
+			break;
+
+		KeDelayExecutionThread(KernelMode, FALSE,(PLARGE_INTEGER) SECOND);
+	}
 }
--- WinLib/WinLibKernel/IOCTLHijack.h
+++ WinLib/WinLibKernel/IOCTLHijack.h
@@ -1,15 +1,26 @@
 #pragma once
 #include "Detour.h"
+#include "Debug.h"
+
+#define SECOND	10000000
+
+typedef NTSTATUS(*FUNC_IRP)(PDEVICE_OBJECT DriverObject, PIRP Irp);
+typedef VOID(*IOCTLCallback)(FUNC_IRP func);
 
 class IOCTLHijack {
 private:
-	DRIVER_OBJECT *victim;
+	DRIVER_OBJECT * victim;
+	IOCTLCallback callback;
+	CHAR idx;
 public:
-	IOCTLHijack(DRIVER_OBJECT *_victim);
+	IOCTLHijack(DRIVER_OBJECT *_victim, CHAR _idx, IOCTLCallback _callback);
 	BOOLEAN isHijackable();
-	/* 
-		==> TODO <== 
+
+	BOOLEAN watch();
+	/*
+		==> TODO <==
 		Parameter: Callback (Called from new KernelThread)
 						gets called if isHijackable gets TRUE
 	*/
+	static VOID WatcherThread(IOCTLHijack* this_ptr);
 };
