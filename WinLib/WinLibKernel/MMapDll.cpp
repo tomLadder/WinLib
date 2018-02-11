6e5f381b8a315cb9984c64d60c5c0b342769fa68
Thomas Leiter <leitert@edu.htl-villach.at>
2018-02-11 18:10:16 +0100

    :construction: added MMapDll base structure

--- WinLib/WinLibKernel/MMapDll.cpp
+++ WinLib/WinLibKernel/MMapDll.cpp
@@ -1,1 +1,128 @@
 #include "MMapDll.h"
+
+#define RVA(m, b) ((PVOID)((ULONG_PTR)(b)+(ULONG_PTR)(m)))
+
+using WinLibKernel::PE::Loader::MMapperDll;
+using WinLibKernel::PE::PEFile;
+
+MMapperDll::MMapperDll(PEFile *peFile) {
+	this->peFile = peFile;
+}
+
+MMapperDll::STATUS MMapperDll::map(PEPROCESS process, PVOID originalEntryPoint, PVOID targetBase, DWORD targetSize) {
+	if (!this->peFile->isValid()) {
+		return PEINVALID;
+	}
+
+	this->payload = ExAllocatePoolWithTag(NonPagedPool, this->peFile->getImageSize(), 'winl');
+	memset(this->payload, 0, this->peFile->getImageSize());
+
+	if (!MMapperDll::mapHeader())
+		return STATUS::FAILED;
+
+	if (!MMapperDll::mapSections())
+		return STATUS::FAILED;
+
+	if (!MMapperDll::fixImports())
+		return STATUS::FAILED;
+
+	if (!MMapperDll::baseRelocation((ULONG_PTR)targetBase))
+		return STATUS::FAILED;
+
+	if (!MMapperDll::writeToProcess(process, targetBase, targetSize))
+		return STATUS::FAILED;
+
+	if (!MMapperDll::patchEntryPoint(originalEntryPoint))
+		return STATUS::FAILED;
+
+	ExFreePool(this->payload);
+
+	return MMapperDll::STATUS::SUCCESS;
+}
+
+bool MMapperDll::mapHeader() {
+	memcpy(this->payload, this->peFile->getDosHeader(), this->peFile->getHeaderSize());
+
+	return true;
+}
+
+bool MMapperDll::mapSections() {
+	for (int i = 0; i < this->peFile->getNumberOfSections(); i++) {
+		PIMAGE_SECTION_HEADER sectionHeader = this->peFile->getSectionHeader(i);
+		CHAR* rawData = this->peFile->getSectionBase(i);
+
+		if (!rawData) {
+			return false;
+		}
+
+		memcpy((CHAR*)this->payload + sectionHeader->VirtualAddress, rawData, sectionHeader->SizeOfRawData);
+	}
+
+	return true;
+}
+
+
+bool MMapperDll::baseRelocation(PVOID targetBase) {
+	ULONG count;
+	ULONG_PTR address;
+	PUSHORT typeOffset;
+	LONGLONG delta;
+
+	delta = (ULONG_PTR)targetBase - this->peFile->getNtHeader()->OptionalHeader.ImageBase;
+	auto dir = (PIMAGE_BASE_RELOCATION)((CHAR*)this->payload + this->peFile->getNtHeader()->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress);
+	auto dirend = dir + this->peFile->getNtHeader()->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size;
+
+	while (dir < dirend && dir->SizeOfBlock > 0) {
+		count = (dir->SizeOfBlock - 8) >> 1;
+		address = (ULONG_PTR)RVA(payload, dir->VirtualAddress);
+		typeOffset = (PUSHORT)(dir + 1);
+		this->processRelocation(address, count, typeOffset, delta);
+
+		dir = dir + dir->SizeOfBlock;
+	}
+
+	return true;
+}
+
+PIMAGE_BASE_RELOCATION MMapperDll::processRelocation(ULONG_PTR address, ULONG count, PUSHORT typeOffset, LONGLONG delta) {
+	SHORT offset;
+	USHORT type;
+	PUSHORT shortPtr;
+	PULONG longPtr;
+	PULONGLONG longLongPtr;
+
+	for (ULONG i = 0; i < count; i++) {
+		offset = *typeOffset & 0xFFF;
+		type = *typeOffset >> 12;
+		shortPtr = (PUSHORT)(RVA(address, offset));
+
+		switch (type) {
+		case IMAGE_REL_BASED_DIR64:
+			longLongPtr = (PUINT64)RVA(address, offset);
+			*longLongPtr = *longLongPtr + delta;
+			break;
+		case IMAGE_REL_BASED_HIGHLOW:
+			longPtr = (PULONG)RVA(address, offset);
+			*longPtr = *longPtr + (delta & 0xFFFFFFFF);
+			break;
+		default:
+			return (PIMAGE_BASE_RELOCATION)NULL;
+		}
+
+		typeOffset++;
+	}
+
+	return (PIMAGE_BASE_RELOCATION)typeOffset;
+}
+
+bool MMapperDll::fixImports() {
+	return true;
+}
+
+bool MMapperDll::writeToProcess(PEPROCESS process, PVOID targetBase, DWORD targetSize) {
+	return true;
+}
+
+bool patchEntryPoint(PVOID originalEntryPoint) {
+	return true;
+}
--- WinLib/WinLibKernel/MMapDll.h
+++ WinLib/WinLibKernel/MMapDll.h
@@ -1,1 +1,34 @@
 #pragma once
+#include "PEFile.h"
+
+namespace WinLibKernel {
+	namespace PE {
+		namespace Loader {
+			class MMapperDll {
+			public:
+				enum STATUS {
+					SUCCESS,
+					FAILED,
+					ACCESSDENIED,
+					PEINVALID
+				};
+
+				MMapperDll(PEFile *peFile);
+
+				STATUS map(PEPROCESS process, PVOID originalEntryPoint, PVOID targetBase, DWORD targetSize);
+			private:
+				PEFile * peFile;
+				PVOID payload;
+
+				bool mapHeader();
+				bool mapSections();
+				bool baseRelocation(PVOID targetBase);
+				bool fixImports();
+				bool writeToProcess(PEPROCESS process, PVOID targetBase, DWORD targetSize);
+				bool patchEntryPoint(PVOID originalEntryPoint);
+
+				PIMAGE_BASE_RELOCATION processRelocation(ULONG_PTR address, ULONG count, PUSHORT typeOffset, LONGLONG delta);
+			};
+		}
+	}
+}
